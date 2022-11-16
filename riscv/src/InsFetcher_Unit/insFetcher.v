eb834cfa2e7191a4f5da2bb6aa591908bc371c53
JerryGJX <1422765817@qq.com>
2022-11-16 15:00:40 +0800

    [IF & IC] initial commit

--- riscv/src/InsFetcher_Unit/insFetcher.v
+++ riscv/src/InsFetcher_Unit/insFetcher.v
@@ -6,5 +6,58 @@ module insFetcher(
     input wire rst,
     input wire rdy,
 
-    
+    //full signal
+    input wire full_flag,
+
+    //from iCache
+    input wire in_from_iCache_hit,
+    input wire [`INSTRUCTION_TYPE ] in_from_iCache_ins,
+    //to iCache
+    output wire out_to_iCache_valid,
+    output wire [`ADDRESS_TYPE ] out_to_iCache_addr,
+
+    //from_predictor
+    input wire in_from_predictor_valid,
+    input wire [`ADDRESS_TYPE ] in_from_predictor_addr,
+    //to_predictor
+    output wire out_to_predictor_valid,
+    output wire [`ADDRESS_TYPE ] out_to_predictor_addr,
+
+    //to predictor
+    output wire out_to_predictor_valid,
+    output wire [`ADDRESS_TYPE ] out_to_predictor_addr,
+    //from predictor
+    input wire in_from_predictor_valid,
+    input wire if_jump,
+    input wire [`ADDRESS_TYPE ] in_from_predictor_jump_imm,
+
+    //to issue
+    output wire out_to_dispatcher_valid,
+    output wire [`INSTRUCTION_TYPE ] out_to_dispatcher_ins
+
 );
+
+    reg [`ADDRESS_TYPE ] PC;
+
+    always @(posedge clk) begin
+        if (rst) begin
+            PC <= 0;
+            out_to_iCache_valid <= 0;
+            out_to_iCache_addr <= `BLANK_ADDR;
+        end
+        else if(rdy) begin //CPU pending
+        end
+        else begin
+            if(in_from_iCache_hit && full_flag != `FALSE ) begin
+                PC <= PC + (if_jump ? in_from_predictor_jump_imm : NEXT_PC );
+                out_to_dispatcher_valid <= ENABLE;
+                out_to_dispatcher_ins <= in_from_iCache_ins;
+            end
+            else begin
+                out_to_dispatcher_valid <= DISABLE;
+            end
+        end
+    end
+
+
+endmodule : insFetcher
--- riscv/src/definition.v
+++ riscv/src/definition.v
@@ -1,2 +1,17 @@
-`define  Enable 1'b1
-`define  Disable 1'b0
+`define TRUE 1'b1
+`define FALSE 1'b0
+
+//wire status
+`define ENABLE 1'b1
+`define DISABLE 1'b0
+
+//element type
+`define INSTRUCTION_TYPE 31:0
+`define ADDRESS_TYPE 31:0
+
+//
+`define BLANK_INS 32'h0
+`define BLANK_ADDR 32'h0
+
+
+`define NEXT_PC 32'h4
